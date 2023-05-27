4af8e2b622938c7a389e32a11bd8ae1824f4a281
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-05-27 15:41:18 +0530

    2nd Commit: settig up Express router

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//to know this has loaded
+console.log('router loaded');
+
+
+
+
+//to be able to use in index.js
+module.exports = router;
