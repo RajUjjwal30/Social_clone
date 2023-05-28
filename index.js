9c4d4fbfa175f69020e37bdb7d04cff1bc7c8e08
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-05-29 00:35:29 +0530

    Creating a layout

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const port = 1000;
 
+//requiring epress-ejs-layouts(we have to keep above routes and views)
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+
+
 //use express router
 //any request that comes in require the index of routes
 app.use('/',require('./routes'));
