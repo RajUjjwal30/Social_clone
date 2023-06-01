857891461163fdffaa2170bf420794395e6fa2fd
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-06-01 14:59:28 +0530

    rendering sign-up and sign-in pages AND creating forms for sign-up and sign-in pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,34 @@
 module.exports.profile = function(req,res){
      return res.render('user_profile',{
           title:"User Profile"
-     });
+     })
 
 }
 //Now above controller is ready to be accessed by router
+//rendering sign-up page
+module.exports.signUp = function(req,res){
+     return res.render('user_sign-up',{
+          title: "Facebook | Sign-Up"
+     })
+}
+
+
+//rendering sign-in page
+module.exports.signIn = function(req,res){
+     return res.render('user_sign-in',{
+          title: "Facebook | Sign-In"
+          //if in title we do not give ("") then we will get 'reference error'
+          //render : file name
+          
+     })
+}
+//get the sign-up data(action for"/users/create)
+module.exports.create = function(req,res){
+     //todo later
+}
+
+//get the sign-in data ,,action for ("users/create-session")
+module.exports.createSession = function(req,res){
+     //todo later
+}
+
