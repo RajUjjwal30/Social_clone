c8f84af84a6ced302c1d2914a03988b0c6800e58
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-06-03 14:04:00 +0530

    Setting up User Sign-Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+//importing user model for accessing functions like findOne
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
      return res.render('user_profile',{
           title:"User Profile"
@@ -24,7 +27,32 @@ module.exports.signIn = function(req,res){
 }
 //get the sign-up data(action for"/users/create)
 module.exports.create = function(req,res){
-     //todo later
+     //check whether password and confirm_password are equal or not
+     //if they are not then redirect back to 'sign-up' page
+     // (req.body)  Reading the body parameter of Form
+     if(req.body.password != req.body.confirm_password){
+          return res.redirect('back');
+     }
+
+     //if password are same ,we will try to find the username with same E-mail(unique) Id
+///mongoose no longer support callbacks , need to convert to promises
+     User.findOne({email: req.body.email}, function(err,user){
+          if(err){console.log('error in finding user in signing up');return}
+
+          //when user is not found
+          if(!user){
+               User.create(req.body,function(err,user){
+                    //req.body --> passed on the request body directly from sign-up.ejs(all the fields)
+                    if(err){console.log('error in creating user while signing up');return}
+
+                    //if not error then user is created
+                    return res.redirect('/users/sign-in')
+               })
+          }else{
+               return res.redirect('back');
+          }
+     });
+
 }
 
 //get the sign-in data ,,action for ("users/create-session")
--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,10 @@ router.get('/profile',usersController.profile);
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 
+//route for creating user
+//using 'post' bcoz riuter is posting this data ,not 'get' bcoz we are not fetching anything from server
+router.post('/create',usersController.create);
+
+
+
 module.exports = router;
