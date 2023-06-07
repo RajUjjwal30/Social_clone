2a1f3251bb6ef53a67876ce8f3c3ce8a1e7b7345
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-06-07 13:27:54 +0530

    Display posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,28 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
+   // console.log(req.cookies);
+   // res.cookie('user_id',25);
+
+//showing posts on the home page
+//Post.find({} --> find all the posts
+
+// Post.find({}, function(err,posts){
+//     return res.render('home',{
+//         title: "Facebook | Home",
+//         posts: posts
+//     });
+// });
+
+//populate the user of each post || exec() is a callback function,shifted the function into exec()
+Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home',{
-    title: "Home"
+        title: "Facebook | Home",
+        posts: posts
+    });
 });
+
+
+
+   
 }
