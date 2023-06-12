439509f58660b589068556865f9e9fd6fa6683ea
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-06-12 14:11:22 +0530

    Deleting a Comment(Authorized)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,3 +1,4 @@
+const { redirect } = require('express/lib/response');
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
@@ -22,6 +23,29 @@ module.exports.create = function(req,res){
                 res.redirect('/');
             });
             
+        }
+    });
+}
+
+
+// req.params -->The req.params property is an object containing properties mapped to the named route “parameters”. For example, if you have the route /student/:id, then the “id” property is available as req.params.id. This object defaults to {}. 
+
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id, function(err, comment){
+        if (comment.user == req.user.id){
+
+            //before deleting comment , save 'PostId' of the comment because we have to delete
+            //comment by finding the PostId of the deleted comment from the Array of comments which is in posts also(refer-post.js(model) )
+            let postId = comment.post;
+            comment.remove();
+
+            // '$pull' is a mongoose(mongoDB) inbuilt function used for extracting out
+            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, function(err, post){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
         }
     });
 }
