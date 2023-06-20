943d17f331c9540cf9ac82cd75fe174f0d6e2ebf
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-06-20 13:11:23 +0530

    Introducing Noty for Flash messages(on sign in/up , invalid username and password)

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -5,21 +5,23 @@ const LocalStrategy = require('passport-local').Strategy;
 
 //Authentication using passport
 passport.use(new LocalStrategy({
-    usernameField: 'email'
+    usernameField: 'email',
+    passReqToCallback: true
+    // passReqToCallback : it alows us to set the first argument as request
 },
 //'done' is callback function which is reporting back to PAssport.js
-function(email,password,done){
+function(req, email,password,done){
     //find the user and establish an identity
     User.findOne({email:email}, function(err,user){
         //second email is the value that is passed on,, first one is from 'Models'(user.js)
         if(err){
-            console.log('Error in finding user --> Passport');
+            req.flash('error',err);
             return done(err);
             //'done' takes 2 arguments, first one is error(the one which we are using right now) and second one is something else
         }
         //if user is not found || Password does not match
         if(!user || user.password != password){
-            console.log('Invalid Username/Password');
+            req.flash('error', 'Invalid Username/Password');
             return done(null,false);
             //done takes 2 arguments but in JS it can work with one as well
             //authentication has not been done so -- false
