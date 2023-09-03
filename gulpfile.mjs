f5e5004b0c12567e92203f1194489ed7881bc622
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-09-03 21:35:01 +0530

    completing GULP : JS and Image minification

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -9,6 +9,13 @@ import gulpuglify from 'gulp-uglify-es';
 const uglify = gulpuglify.default;
 
 
+import imagemin from 'gulp-imagemin';
+import {deleteSync} from 'del';
+
+
+
+
+
 //in gulp we create tasks
 gulp.task('css', function(done){
     console.log('minifying css..');
@@ -20,7 +27,7 @@ gulp.task('css', function(done){
     // made a new folder 'public'. public ->assets>css/images/js
     //and in environment variables changed the ASSET_PATH to "./public/assets"
 
-    return gulp.src('./assets/**/*.css')
+     gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
     .pipe(rev.manifest({
@@ -31,4 +38,44 @@ gulp.task('css', function(done){
     done();
 
 
+});
+
+gulp.task('js',function(done){
+    console.log('minifying js...');
+    gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+     done();
+});
+
+gulp.task('images', function(done){
+    console.log('compressing images...');
+    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+//empty the public/assets directory
+//whenever u are building a project, you need to clear the previous build and build it from scratch
+gulp.task('clean:assets', function(done){
+    deleteSync('./public/assets');
+    done();
+});
+
+gulp.task('build',gulp.series('clean:assets', 'css','js','images'), function(done){
+    console.log('Building assets');
+    done();
 });
